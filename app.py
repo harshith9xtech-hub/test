0a7e21d1e7fc3e9a1e4b3b195fa34a2fa647eb36
root <root@DESKTOP-T68NCF7.localdomain>
2026-03-27 16:45:19 +0400

    updated app.py now

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.get("/")
 def hello():
     return jsonify(
-        message="✨ Welcome to Cloud with Harshith Lets GO✨",
+        message="✨ Welcome to Cloud with Harshith Lets GO build the pipeline ✨",
         tip="Built with Flask, shipped by Jenkins, running in Docker."
         UI="this is my new ui"
     )
